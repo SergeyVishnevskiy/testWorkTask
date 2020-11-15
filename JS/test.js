41ea4363e59ca211752f174f14d14a3090d457ab
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-15 11:23:54 +0200

    test

---
+++ JS/test.js
@@ -0,0 +1,20 @@
+'use script'
+
+//==========проверка на тип===============\\
+
+// const switcher = (data) => {
+//     // console.log(typeof data);
+//     if (typeof data === "string") {
+//         console.log(`Type: string`);
+//     }
+//     if (typeof data === "number"){
+//         console.log(`Type: number`);
+//     }
+//     if (typeof data === "object") {
+//         console.log(`Type: object`);
+//     }
+// }
+// switcher("lkdsgfkadflk");
+//=======================================\\
+
+console.log('object');
