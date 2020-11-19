d93730d448e2517f9bfc0883345fc3877e803242
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-19 14:40:37 +0200

    task1 find letters

--- JS/module05-compendium.js
+++ JS/module05-compendium.js
@@ -1,23 +1,48 @@
-const baseSalary = 30000;
-const overtime = 10;
-const rate = 20;
+// const baseSalary = 30000;
+// const overtime = 10;
+// const rate = 20;
 
-const getWage = (baseSalary, overtime, rate) => {
-  return baseSalary + overtime * rate;
-};
+// const getWage = (baseSalary, overtime, rate) => {
+//   return baseSalary + overtime * rate;
+// };
 
-getWage(baseSalary, overtime, rate);
-console.log(getWage(baseSalary, overtime, rate));
+// getWage(baseSalary, overtime, rate);
+// console.log(getWage(baseSalary, overtime, rate));
 
-const employee = {
-  baseSalary: 30000,
-  overtime: 10,
-  rate: 20,
-  getWage() {
-    return this.baseSalary + this.overtime * this.rate;
-  },
-};
+// const employee = {
+//   baseSalary: 30000,
+//   overtime: 10,
+//   rate: 20,
+//   getWage() {
+//     return this.baseSalary + this.overtime * this.rate;
+//   },
+// };
 
-employee.getWage();
-employee.baseSalary = 10;
-console.log(employee.getWage());
+// employee.getWage();
+// employee.baseSalary = 10;
+// console.log(employee.getWage());
+//===================19.11.2020-practice==============\\
+//===============task1=================\\
+// const string = "Lorem ipsum dolor sit amet";
+
+// const searchLiteral = function (string) {
+//   const literal = "oeaiu";
+//   let total = 0;
+//   const wordsArr = string.split(" ");
+//   const symbolsArr = literal.split("");
+//   console.log(symbolsArr);
+//   for (const word of wordsArr) {
+//     let letters = word.split("");
+//     for (const letter of letters) {
+//       let symbol = literal.includes(letter);
+//       if (symbol) {
+//         total += 1;
+//       }
+//     }
+//   }
+//   return total;
+// };
+// console.log(searchLiteral(string));
+//=====================================\\
+
+//===============task1=================\\
