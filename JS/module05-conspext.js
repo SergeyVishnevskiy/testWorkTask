c1ad2303d3a371e397713081fa0c262e214a87d5
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-19 13:24:40 +0200

    konspext-module

---
+++ JS/module05-conspext.js
@@ -0,0 +1,23 @@
+const baseSalary = 30000;
+const overtime = 10;
+const rate = 20;
+
+const getWage = (baseSalary, overtime, rate) => {
+  return baseSalary + overtime * rate;
+};
+
+getWage(baseSalary, overtime, rate);
+console.log(getWage(baseSalary, overtime, rate));
+
+const employee = {
+  baseSalary: 30000,
+  overtime: 10,
+  rate: 20,
+  getWage() {
+    return this.baseSalary + this.overtime * this.rate;
+  },
+};
+
+employee.getWage();
+employee.baseSalary = 10;
+console.log(employee.getWage());
